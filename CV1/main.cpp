8d5e2c7d46200ae2fb5502fe2bf905dd87b10c4a
鹏飞 朱 <15671143534@163.com>
2023-11-15 17:20:12 +0800

    工作

--- CV1/main.cpp
+++ CV1/main.cpp
@@ -2,15 +2,23 @@
 #include <opencv.hpp>
 #include <general.h>
 #include "cvalgrm.h"
+#include <HalconCpp.h>
+
+using namespace HalconCpp;
+
+using himg = HImage;
 
 int main()
 {
 	cv::Mat img = cv::imread(my::GetFileName(), cv::IMREAD_GRAYSCALE);
-	cv::imshow("1", img);
-	cv::waitKey();
+	/*cv::imshow("1", img);
+	cv::waitKey();*/
 
-	auto rslt = my::fitEllipseHalcon(img);
-	cv::Mat ellipsimg = my::drawEllipseOnImage(img, rslt);
-	cv::imshow("rslt", ellipsimg);
-	cv::waitKey();
+	//auto rslt = my::fitEllipseHalcon(img);
+	//cv::Mat ellipsimg = my::drawEllipseOnImage(img, rslt);
+	//cv::imshow("rslt", ellipsimg);
+	//cv::waitKey();
+	himg himg("bytr", img.cols, img.rows, img.data);
+	himg.DispImage(HWindow());
+
 }
